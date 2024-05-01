12ffd88d323c1db0e5ad1e6a984577c7d93f4cc3
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 13:55:46 +1000

    Higher order function operate: calls basic functions to operate numbers.

--- calc.js
+++ calc.js
@@ -18,6 +18,11 @@ function divide(a,b) {
     return a/b
 }
 
-function operate(operator, a, b) {
-
+function operate(operator, firstNum, secondNum) {
+    switch(operator) {
+        case "+": return(add(firstNum, secondNum));
+        case "-": return(subtract(firstNum, secondNum));
+        case "*": return(multiply(firstNum, secondNum));
+        case "/": return(divide(firstNum, secondNum));
+    }
 }
